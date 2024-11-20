f246ba28dc38253fed5f23c2592eef9bd911d62b
MaiThiHongNgoc <hongngoc200509@gmail.com>
2024-11-20 18:50:54 +0700

    update

--- backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ backend/obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d558ddb79fb9800b6fba84874c0714cd4130c2b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+669ca8b598ab21b98fbe67ac0ea2b5676f5b260c")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
